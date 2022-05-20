77ba56ae8722c9799047654bf64289981d0afe1c
SrdjanBeric <srdjan.beric123@gmail.com>
2022-05-20 14:01:28 +0200

    Refactor flower container

--- flowerspot/src/App.js
+++ flowerspot/src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ExplorePanel from "./components/ExplorePanel";
 import FlowerCard from "./components/FlowerCard";
 import FlowerContainer from "./components/FlowerContainer";
 import Navigationbar from "./components/Navigationbar";
 import "./components/style/Main.css";
+import axios from "axios";
 
 function App() {
+    const [flowers, setFlowers] = useState([]);
+
+    useEffect(() => {
+        getFlowers();
+    }, []);
+
+    const getFlowers = () => {
+        axios
+            .get("https://flowrspot-api.herokuapp.com/api/v1/flowers?page=1")
+            .then((response) => {
+                const allFlowers = response.data.flowers;
+                setFlowers(allFlowers);
+                console.log(response.data.flowers);
+            })
+            .catch((error) => console.error(`Error: ${error}`));
+    };
+
+
     return (
         <div className="main-page">
             <Navigationbar />
             <ExplorePanel />
-            <FlowerContainer className="flowers-container"/>
+            {console.log('HERE:', flowers)}
+            <FlowerContainer flowers={flowers} className="flowers-container" />
             {/* <div className="flowers-container">
                 <FlowerCard />
                 <FlowerCard />
--- flowerspot/src/components/FlowerContainer.js
+++ flowerspot/src/components/FlowerContainer.js
@@ -2,32 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import FlowerCard from "./FlowerCard";
 
-function FlowerContainer() {
-    const [flowers, setFlowers] = useState([]);
-
-    useEffect(() => {
-        getFlowers();
-    }, []);
-
-    const getFlowers = () => {
-        axios
-            .get("https://flowrspot-api.herokuapp.com/api/v1/flowers?page=1")
-            .then((response) => {
-                const allFlowers = response.data.flowers;
-                setFlowers(allFlowers)
-                console.log(response.data.flowers);
-            })
-            .catch((error) => console.error(`Error: ${error}`));
-    };
-
-    const renderFlowers = flowers.map((flower)=>{
+function FlowerContainer(flowers) {
+    const renderFlowers = flowers.flowers.map((flower)=>{
         return (
-            <FlowerCard info={flower}/>
+            <div key={flower.id}>
+                <FlowerCard info={flower}/>
+            </div>
         )
     })
     return (
     <div className="flowers-container">
         {renderFlowers}
+        {/* {console.log(flowers)} */}
     </div>
     );
 }
