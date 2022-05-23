81e0a0ed5694d642c550e9d9e262e2768f4d24ea
SrdjanBeric <srdjan.beric123@gmail.com>
2022-05-23 10:40:14 +0200

    Registration style

--- flowerspot/src/components/Registration.js
+++ flowerspot/src/components/Registration.js
@@ -5,21 +5,37 @@ function Registration() {
     return (
         <div className="registration-card">
             <div className="registration-content">
-                <h3>Create an Account</h3>
-                <input
-                    className="registration-input"
-                    placeholder="First Name"
-                />
-                <input className="registration-input" placeholder="Last Name" />
-                <input
-                    className="registration-input"
-                    placeholder="Date of Birth"
-                />
-                <input
-                    className="registration-input"
-                    placeholder="Email Adress"
-                />
-                <input className="registration-input" placeholder="Password" />
+                <div className="registration-title">Create an Account</div>
+                <div className="first-row">
+                    <div className="registration-input-div">
+                        <input className="registration-input short-input" />
+                        <span class="registration-input-description">
+                            First Name
+                        </span>
+                    </div>
+                    <div className="registration-input-div">
+                        <input className="registration-input short-input" />
+                        <span class="registration-input-description">
+                            First Name
+                        </span>
+                    </div>
+                </div>
+                <div className="registration-input-div">
+                    <input className="registration-input" />
+                    <span class="registration-input-description">
+                        Date of Birth
+                    </span>
+                </div>
+                <div className="registration-input-div">
+                    <input className="registration-input" />
+                    <span class="registration-input-description">
+                        Email Adress
+                    </span>
+                </div>
+                <div className="registration-input-div">
+                    <input className="registration-input" />
+                    <span class="registration-input-description">Password</span>
+                </div>
                 <button className="registration-button">Create Account</button>
             </div>
         </div>
