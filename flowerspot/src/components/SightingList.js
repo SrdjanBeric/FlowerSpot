0a8dd5fe533c92d1acd645e6a0aa6b9473dc9f2c
SrdjanBeric <srdjan.beric123@gmail.com>
2022-06-01 10:58:18 +0200

    Sighting list header

--- flowerspot/src/components/SightingList.js
+++ flowerspot/src/components/SightingList.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import SightingContainer from "./SightingContainer";
 import "./style/SightingList.css";
 import { fetchSightings } from "../redux/sightings/sightingsActions";
+import backgroundImage from "../images/flower-detail-background.png";
 
 function SightingList({ fetchSightings, sightingsData }) {
     let { page } = useParams();
@@ -14,6 +15,25 @@ function SightingList({ fetchSightings, sightingsData }) {
 
     return (
         <div>
+            <div className="sighting-list-header">
+                <img
+                    className="sighting-list-background-img"
+                    src={backgroundImage}
+                />
+                <div className="sighting-list-header-content">
+                    <div className="sighting-list-header-text">
+                        <p className="sighting-list-header-title">
+                            Sighting List
+                        </p>
+                        <p className="sighting-list-header-description">
+                            Explore between more than 8.427 sightings
+                        </p>
+                    </div>
+                    <button className="sighting-list-header-button">
+                        + Add New Sighting
+                    </button>
+                </div>
+            </div>
             <SightingContainer sightings={sightingsData} />
         </div>
     );
